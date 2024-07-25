9f8127a4cf3c75de28f692d883c9c5dca1695cef
Pushkar Tatiya <pushkart2@gmail.com>
2024-07-25 13:11:26 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# snipe-queue
+# Snipe Queue System
+
+# Features:
+- Priority Queue based on discord roles
+- Stackable priority based on roles. If you have two roles one with 10 and other with 20, you will have 30 priority.
+- Optimized
+- Easy to use
+
+# Installation
+
+- Download the script. Make sure its named `snipe-queue`
+- Ensure the script in your server.cfg `ensure snipe-queue`
+- Configure the script. Check the config and change it to your liking.
+- Add your discord bot token and server id in config_discord.lua
+- Add your discord role ids along with the points system in config_discord.lua
---
+++ language.lua
@@ -0,0 +1,13 @@
+Lang = {
+    please_wait = 'Please wait before we initialize your connection',
+    checking_identifiers = 'Checking for identifiers...',
+    ids_doesnt_exist = 'Couldnt fetch identifiers',
+    id_doesnt_exist = '%s doesnt exist',
+    timed_out = 'You were timed out of session',
+    could_not_connect = 'Connection failed!',
+    not_whitelisted = 'You are not whitelisted!',
+    already_in_queue = 'You were already in queue, please reconnect',
+    joining_now = 'Joining...',
+    checking_roles = 'Checking Roles...',
+    join_discord = 'Please join discord https://discord.gg/AeCVP2F8h7',
+}
